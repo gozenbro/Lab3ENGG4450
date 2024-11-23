5e2af34b957e0a18ec9c948cfeaca24dcac58ede
gozenbro <96926186+gozenbro@users.noreply.github.com>
2024-11-22 22:22:35 -0500

    Add files via upload

---
+++ BubbleSortTest.java
@@ -0,0 +1,23 @@
+package com.jwetherell.algorithms.sorts.test;
+
+import com.jwetherell.algorithms.sorts.BubbleSort;
+import org.junit.Test;
+import org.junit.Assert;
+import junit.framework.TestCase;
+public class BubbleSoftTestTest {
+    @Test
+    public void testSort() {
+        int unsorted[]= 0;
+        sort(T[] unsorted);
+        int index1=0;
+        int index2=0;
+        void swap(int index1, int index2, T[] unsorted)
+    }
+    public void testSort2() {
+        int unsorted[]= 1000000000000000000000000000000000000000000000000000000000000;
+        sort(T[] unsorted);
+        int index1=1000000000000000000000000000000000000000000000000000000000000;
+        int index2=1000000000000000000000000000000000000000000000000000000000000;
+        void swap(int index1, int index2, T[] unsorted)
+    }
+}
---
+++ QuickSortTest.java
@@ -0,0 +1,23 @@
+package com.jwetherell.algorithms.sorts.test;
+
+import com.jwetherell.algorithms.sorts.QuickSort;
+import org.junit.Test;
+import org.junit.Assert;
+import junit.framework.TestCase;
+public class QuickSoftTest {
+    @Test
+    public void testSort3() {
+        int unsorted[]= 0;
+        sort(T[] unsorted);
+        int index1=0;
+        int index2=0;
+        void swap(int index1, int index2, T[] unsorted)
+    }
+    public void testSort4() {
+        int unsorted[]= 1000000000000000000000000000000000000000000000000000000000000;
+        sort(T[] unsorted);
+        int index1=1000000000000000000000000000000000000000000000000000000000000;
+        int index2=1000000000000000000000000000000000000000000000000000000000000;
+        void swap(int index1, int index2, T[] unsorted)
+    }
+}
